69d21bf5329d578860f9df8ee0fd50aac9002c26
LoganjwSim <j1sim234@gmail.com>
2023-11-09 16:51:25 +0900

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log(`현재 count 값 : ${count}`);
+  }, [count]);
+
+  return (
+    <div className="bg-red-100 text-center">
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <span className="mx-4">{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
 };
 
 export default App;
+
+//useEffect(() => {
+//   if (count === 0) {
+//    console.log("Hello, useEffect");
+//  } else {
+//    console.log(`count? ${count}`);
+//  }
+// }, [count]);
