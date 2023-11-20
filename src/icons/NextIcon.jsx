4f1ccf5e930ca8970f3486eef95d5998f538b3c9
LoganjwSim <j1sim234@gmail.com>
2023-11-20 16:30:08 +0900

    schedule section

--- src/icons/NextIcon.jsx
+++ src/icons/NextIcon.jsx
@@ -1,20 +1,20 @@
-const NextIcon = () => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        className="h-4 w-4"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M9 5l7 7-7 7"
-        ></path>
-      </svg>
-    );
-  };
-  
-  export default NextIcon;
+const NextIcon = ({ size }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className={size ? size : "w-4 h-4"}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M9 5l7 7-7 7"
+      ></path>
+    </svg>
+  );
+};
+
+export default NextIcon;
